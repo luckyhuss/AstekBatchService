20c644cc01c1298d28b6540e681ddabeed148bf2
Anwar Buchoo <luckyhuss@msn.com>
2016-11-07 08:21:58 +0100

    Update service path

--- register/install service.cmd
+++ register/install service.cmd
@@ -1,5 +1,5 @@
 for %%i in ("%~dp0..") do set "folder=%%~fi"
 echo %folder%
-SET parentPath=%folder%
+SET parentPath=%folder%\bin\Debug
 sc create AstekBatchService binPath= "%parentPath%\AstekBatchService.exe" DisplayName= "Astek Batch Service" start= auto
 sc description AstekBatchService "Lauches jobs at regular interval for ASTEK"
